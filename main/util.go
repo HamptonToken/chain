0fc04ee8d9e1a47963fe75d1b9b82d1a4958422c
Song Liu <soliu@fb.com>
2022-01-06 17:53:46 -0800

    add a util file

---
+++ main/util.go
@@ -0,0 +1,27 @@
+package common
+
+import (
+	"encoding/binary"
+	"os"
+)
+
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+func UInt64ToBytes(i uint64) []byte {
+	var buf = make([]byte, 8)
+	binary.BigEndian.PutUint64(buf, i)
+	return buf
+}
+
+func BytesToUInt64(buf []byte) uint64 {
+	return binary.BigEndian.Uint64(buf)
+}
