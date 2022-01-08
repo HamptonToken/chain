6902c4f96bab66e7f1ccdc08631b0ce02aac3262
Harrison <van20052005@hotmail.com>
2022-01-07 17:32:42 -0800

    Update helper.go

--- common/helper.go
+++ common/helper.go
@@ -5,7 +5,7 @@ import (
 
         "github.com/tendermint/tendermint/abci/types"
 )
-
+/* public key in %X, base 16 with upper-case letters for A-F*/
 func MakeValSetChangeTx(pubkey types.PubKey, power int64) []byte {
         return []byte(fmt.Sprintf("val:%X/%d", pubkey.Data, power))
 }
